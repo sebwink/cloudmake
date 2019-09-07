9a4d412823e79493f1c3e2580c76592a4bb80ac5
sebwink <sebwink@gmx.net>
2019-09-07 22:35:13 +0200

    clean up .cloudmake dirs

--- examples/actix-app/.cloudmake/setup-regsitry.sh
+++
@@ -1,6 +0,0 @@
-container_id=$1
-json='{ "insecure-registry": ["cloudmake-registry:5000"] }'
-
-docker exec -i ${container_id} /bin/sh -c "mkdir -p /etc/docker && jq -n '${json}' > /etc/docker/daemon.json"                                                       
-docker exec ${container_id} systemctl daemon-reload                                                                                                                 
-docker exec ${container_id} systemctl restart docker
